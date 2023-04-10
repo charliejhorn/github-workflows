464577c6f7919f59e4dad007b64be681391b0200
Charlie <chazzahorn@gmail.com>
2023-04-10 12:21:21 +0100

    added "upload artifact" to nodejs.yaml

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
+const port = 8000;
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/' ,(req, res) => {
-  res.sendFile('index.html', {'root': 'public'})
+  res.send('Hello World! Goodbye')
+  // res.sendFile('index.html', {'root': 'public'})
 });
 
-app.listen(9000, function () {
-  console.log(`App running on port ${ this.address().port }`);
+app.listen(port, () => {
+  // console.log(`App running on port ${ this.address().port }`);
+  console.log(`App running on port ${ port }`);
 });
